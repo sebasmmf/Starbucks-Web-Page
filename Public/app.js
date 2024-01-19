40a084532384a60f829a71f2dab2b4cd454ca299
sebasmmf <sebastian.mart.f@gmail.com>
2024-01-19 00:25:43 -0500

    Making a transition for the mobile-menu

--- Public/app.js
+++ Public/app.js
@@ -1,10 +1,17 @@
+const body = document.querySelector('body');
 const menu = document.querySelector('.mobile-menu');
 const btnOpenMenu = document.querySelector('.menu-icon');
 
-btnOpenMenu.addEventListener('click', openMenu)
+btnOpenMenu.addEventListener('click', openMenu);
 
 function openMenu() {
-    menu.style.display = 'flex';
+
+    menu.style.width = '65%';
+    body.style.overflow = 'hidden';
+
     const btnCloseMenu = document.querySelector('.close-menu');
-    btnCloseMenu.addEventListener('click', () => menu.style.display = 'none');
+    btnCloseMenu.addEventListener('click', () => {
+        menu.style.width = '0%';
+        body.style.overflow = 'visible';
+    });
 }
