249e74ce7ea601bcfac0d3da9a5c2b177f738f47
sebasmmf <sebastian.mart.f@gmail.com>
2024-01-22 18:25:28 -0500

    New mobile-menu transition

--- Public/app.js
+++ Public/app.js
@@ -1,17 +1,20 @@
 const body = document.querySelector('body');
 const menu = document.querySelector('.mobile-menu');
 const btnOpenMenu = document.querySelector('.menu-icon');
+const btnCloseMenu = document.querySelector('.close-menu');
 
 btnOpenMenu.addEventListener('click', openMenu);
+btnCloseMenu.addEventListener('click', closeMenu);
 
 function openMenu() {
-
     menu.style.width = '65%';
+    menu.style.opacity = '1';
     body.style.overflow = 'hidden';
 
-    const btnCloseMenu = document.querySelector('.close-menu');
-    btnCloseMenu.addEventListener('click', () => {
-        menu.style.width = '0%';
-        body.style.overflow = 'visible';
-    });
+}
+
+function closeMenu() {
+    menu.style.width = '0';
+    menu.style.opacity = '0';
+    body.style.overflow = 'visible';
 }
